acc73ab2b17e6fc840ce27c3c5fe937d908ae6f5
Fady Morcos <fadymorcos22@gmail.com>
2025-07-23 19:04:54 -0400

    Server set to local

--- client/app.config.js
+++ client/app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: "resistance",
     version: "1.0.0",
     extra: {
-      SOCKET_SERVER: "https://resistnce-game-srver-app.store",
+      SOCKET_SERVER: "http://localhost:3000",
     },
   },
 };
